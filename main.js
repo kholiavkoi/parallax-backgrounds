974e15a34c39b753e2dfe57f6d4c140654e24f90
kholiavkoi <76649658+kholiavkoi@users.noreply.github.com>
2023-06-10 20:53:06 +0300

    added layers

--- main.js
+++ main.js
@@ -21,15 +21,61 @@ backgroundLayer4.src = 'layer-4.png'
 const backgroundLayer5 = new Image()
 backgroundLayer5.src = 'layer-5.png'
 
-let x = 0
-let x2 = 2400
+window.addEventListener('load', function() {
+	const slider = document.getElementById('slider')
+	slider.value = gameSpeed
 
-function animate() {
-	ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
-	ctx.drawImage(backgroundLayer4, x, 0)
-	if (x < -2400) x = 1600
-	else x -= gameSpeed
-	requestAnimationFrame(animate)
-}
+	const showGameSpeed = document.getElementById('showGameSpeed')
+	showGameSpeed.innerHTML = gameSpeed.toString()
 
-animate()
+	slider.addEventListener('change', function(e) {
+		gameSpeed = e.target.value
+		showGameSpeed.innerHTML = e.target.value
+	})
+
+
+	class Layer {
+		constructor(image, speedModifier) {
+			this.x = 0
+			this.y = 0
+			this.width = 2400
+			this.height = 700
+			this.image = image
+			this.speedModifier = speedModifier
+			this.speed = gameSpeed * this.speedModifier
+		}
+
+		update() {
+			this.speed = gameSpeed * this.speedModifier
+			if (this.x <= -this.width) {
+				this.x = 0
+			}
+			this.x = Math.floor(this.x - this.speed)
+		}
+
+		draw() {
+			ctx.drawImage(this.image, this.x, this.y, this.width, this.height)
+			ctx.drawImage(this.image, this.x + this.width, this.y, this.width, this.height)
+		}
+	}
+
+	const layer1 = new Layer(backgroundLayer1, 0.2)
+	const layer2 = new Layer(backgroundLayer2, 0.4)
+	const layer3 = new Layer(backgroundLayer3, 0.6)
+	const layer4 = new Layer(backgroundLayer4, 0.8)
+	const layer5 = new Layer(backgroundLayer5, 1)
+
+	const gameObjects = [layer1, layer2, layer3, layer4, layer5]
+
+	function animate() {
+		ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
+		gameObjects.forEach(obj => {
+			obj.update()
+			obj.draw()
+		})
+		requestAnimationFrame(animate)
+	}
+
+	animate()
+})
+
